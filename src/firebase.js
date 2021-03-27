0ce1cba491a673ab812616c3d6be2f5f47d23992
Alamgir Ahamed Saif <36258593+alamgir72435@users.noreply.github.com>
2021-03-27 17:19:24 +0600

    initialize tinder clone

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCqlR6KQxzeIi0BsbPV3OKxZ1oRTg5p2qQ",
+  authDomain: "tinder-clone-by-saif.firebaseapp.com",
+  projectId: "tinder-clone-by-saif",
+  storageBucket: "tinder-clone-by-saif.appspot.com",
+  messagingSenderId: "986528384806",
+  appId: "1:986528384806:web:36d3221032c8d2ad2763e7",
+};
